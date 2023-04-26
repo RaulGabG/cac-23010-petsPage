151a20eeb1c86f36ea3e2f473cf12789b49ff486
RaulGabG <raulgguanca@gmail.com>
2023-04-26 17:49:52 -0300

    added content to description in details page

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ const allProducts = {
       image:
         "https://media.istockphoto.com/id/1215180165/es/foto/concepto-de-cuidado-juego-y-entrenamiento-de-mascotas.jpg?s=612x612&w=is&k=20&c=WNoifStb1o3GnLyPE3kz0VqRIycm8fMJ9r0xdMYTiek=",
       description: "Para perritos traviesos",
+      descripExt:
+        "Para perritos traviesos. Incluye una cuerda con nudos en los extremos, dos aros de goma con textura de puntas y un hueso de goma.",
       price: 3000,
     },
     {
@@ -25,6 +27,8 @@ const allProducts = {
       image:
         "https://media.istockphoto.com/id/920339338/es/foto/alimentos-para-mascotas-en-un-recipiente-azul.jpg?s=612x612&w=is&k=20&c=44FsVQgBNErITLgHGW5ONC53dy5uW1CxwQ2fOscJUd0=",
       description: "Para que sepa cuánto lo quieres",
+      descripExt:
+        "Para que sepa cuánto lo quieres. Ideal para su comida diaria, tamaño para perro raza pequeña o cachorro / gato.",
       price: 2000,
     },
     {
@@ -33,6 +37,8 @@ const allProducts = {
       image:
         "https://media.istockphoto.com/id/876583404/es/foto/set-de-juguetes-para-perros-bola-de-juguete-y-rosa-de-perro-algod%C3%B3n-colorido-sobre-un-fondo.jpg?s=612x612&w=is&k=20&c=DjhQ0gIc5zbxHIYxrqAmCcZCCZKHID7PgtGHEGyhefs=",
       description: "Para que juege sin parar",
+      descripExt:
+        "Para que juege sin parar. Incluye cuerda de tela con lazo, pelota de goma con textura de puntas ¡y unos snacks para que la diversión nunca termine!",
       price: 2500,
     },
     {
@@ -41,6 +47,8 @@ const allProducts = {
       image:
         "https://media.istockphoto.com/id/1069220754/es/foto/suministros-de-mascotas-y-alimentos-en-forma-de-coraz%C3%B3n-sobre-fondo-de-madera.jpg?s=612x612&w=is&k=20&c=I08efNT8DRPfpzkPVpNP-wN7NkWa6oaD8aQGQvJOmBc=",
       description: "Para que se distraiga mientras le cepillas",
+      descripExt:
+        "Para que se distraiga mientras le cepillas. La cuerda posee un lazo en un extremo para derla fija, ¡mientras tira de ella aprovechá para cepillarle!",
       price: 3000,
     },
     {
@@ -49,6 +57,8 @@ const allProducts = {
       image:
         "https://cdn.pixabay.com/photo/2016/03/04/23/52/foam-balls-1236930_960_720.jpg",
       description: "La diversión no tiene fin",
+      descripExt:
+        "La diversión no tiene fin. Ideal para gatitos llenos de energía. ¡No se la podrás quitar!",
       price: 700,
     },
   ],
